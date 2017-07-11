b18431ebc2f8475684974ad0544bc69230903447
mingong <gongmingqm10@foxmail.com>
2017-07-11 17:29:56 +0800

    Build the first react component

--- app/index.js
+++ app/index.js
@@ -1,2 +1,10 @@
-var textNode = document.createTextNode('Hello React 0 to 1');
-document.body.appendChild(textNode);
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class FirstComponent extends React.Component {
+	render() {
+		return <div> Hello, {this.props.message} </div>;
+	}
+}
+
+ReactDOM.render(<FirstComponent message="Ming"/>, document.getElementById('app'));
