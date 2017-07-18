6500368fdbb8360599b0e54d38e662177514ea05
mingong <gongmingqm10@foxmail.com>
2017-07-18 16:22:13 +0800

    End of session 1

--- app/index.js
+++ app/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
 class FirstComponent extends React.Component {
 	render() {
 		return <div> Hello, {this.props.message} </div>;
--- webpack.config.js
+++ webpack.config.js
@@ -8,8 +8,22 @@ module.exports = {
 		path: path.resolve(__dirname, 'dist')
 	},
 	module: {
-		rules: [
-			{test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'}
+		rules: [{
+				test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'
+			}, 
+			{
+		        test: /\.scss$/,
+		        use: [{
+		          loader: "style-loader"
+		        }, {
+		          loader: "css-loader",
+		          options: { modules: true, localIdentName: '[name]__[local]--[hash:base64:5]' }
+		        }, {
+		          loader: "postcss-loader"
+		        }, {
+		          loader: "sass-loader"
+		        }]
+	        }
 		]
 	},
 	plugins: [
